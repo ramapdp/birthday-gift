630da969490ebcb6b079a816f89b71371c9b7e8a
Ramapdp <ramasatelit484@gmail.com>
2024-11-13 09:24:15 +0700

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { UserConfigExport } from 'vite';
-import { configDefaults } from 'vitest/config';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { UserConfigExport } from "vite";
+import { configDefaults } from "vitest/config";
 
 const config: UserConfigExport = defineConfig({
   plugins: [react()],
+  base: "/special-gift/",
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setupTests.ts',
-    exclude: [...configDefaults.exclude, 'node_modules/**']
+    environment: "jsdom",
+    setupFiles: "./src/test/setupTests.ts",
+    exclude: [...configDefaults.exclude, "node_modules/**"],
   },
 });
 
